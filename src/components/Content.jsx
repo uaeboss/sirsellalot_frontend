173971f33c6595eff4311955cf70d519c36f0c8c
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2023-10-25 14:46:24 +0200

    add routes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+
 const Content = () => {
   return (
     <div className="content">
-      <p>Content</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        {/* <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="user" element={<User />} />
+        <Route path="category" element={<Category />} />
+        <Route path="messenger" element={<Messenger />} /> */}
+      </Routes>
     </div>
   );
 };
