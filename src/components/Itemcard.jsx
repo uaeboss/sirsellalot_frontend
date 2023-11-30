d193a923a2bc0d28ffec6b18a6d5ecdd8ce9f117
uaeboss <jeskemal@gmail.com>
2023-11-30 14:51:55 +0100

    add itemcard.jsx

---
+++ src/components/Itemcard.jsx
@@ -0,0 +1,11 @@
+import "../styles/Itemcard.css";
+
+const Itemcard = () => {
+    return (
+      <div className="banner1">
+        <p>Itemcard</p>
+      </div>
+    );
+  };
+  
+  export default Itemcard;
